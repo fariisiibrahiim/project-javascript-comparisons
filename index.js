66143e7737decbe40ba5def66f7644e6d96a460c
fariisiibrahiim <farisibrahmi@gmail.com>
2019-11-05 15:56:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//object
+const names = {
+  //string
+  author_name: "Muhammad Faris Ibrahim",
+  //prompt
+  client_name: prompt("Enter your name here")
+};
+
+let greetings = `Hello ${names.client_name}!`;
+//array and binary addition
+let colors =
+  "These are my favorite colors" + "\n" + ["Blue", "Pink", "red", "white"];
+
+console.log(names);
+console.log(greetings);
+console.log(colors);
